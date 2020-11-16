4eecab67ac1b96f5c9f426369aa9187037848a12
FabianKamp <fabian.kamp@fu-berlin.de>
2020-11-16 11:06:47 +0100

    Edit optimization

--- optimization.py
+++ optimization.py
@@ -17,6 +17,7 @@ class optimization():
         Initialiazes first generation. 
         :params Empiric Data File
         """
+        print('Setting up optimization.')
         assert method in ['FC', 'CCD'], 'Fitting must be FC or CCD'
         self.method = method  
         # Load empirical Data
@@ -66,23 +67,26 @@ class optimization():
         self.toolbox = base.Toolbox()
         self._initializePopulation()
         # genetic algorithm settings
-        self.NPopinit = 30
+        self.NPopinit = 20
+        self.NPop = 10
+        self.NGen = 10 
         self.crossPortion = 0.4
         self.mutPortion = 0.4
         self.elitPortion = 0.1
-        self.CxPB = .75 # Crossing Over probability
-        self.MutPB = .75 # Mutation Probability
-        
-        # Sigma of gaussian distribution with which attributes are mutated
-        self.MutSigma = [(up-low)/4 for low,up in self.ParamRanges.values()]
 
+        self.cx_s = 1.5
+        self.mut_s = 1.5
+        
         # Genetic Operations
         # Register genetic operators with default arguments in toolbox
+        self.toolbox.register("selBest", tools.selBest) 
+        self.toolbox.register("selRank", self.selRank) 
+        
         self.toolbox.register("model", self._parallel_wc) 
-        self.toolbox.register("select", tools.selBest)                  
-        self.toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=self.MutSigma, indpb=0.5)                      
-        self.toolbox.register("mate", tools.cxUniform, indpb=0.5)
-
+        self.toolbox.register("evaluate", self.getFit)                
+        
+        self.toolbox.register("mutate", self.mutate)                      
+        self.toolbox.register("mate", self.crossover)
 
     def _initializePopulation(self):
         """
@@ -122,90 +126,108 @@ class optimization():
         to the empirical data
         """
         print('Started Optimization.')
-        Generation = 0
         # Create the Population with n individuals
         self.pop = self.toolbox.population(n=self.NPopinit)
 
         # Compute Wilson Cowan Model for individuals in initial population
         with Pool(processes=20) as p:
             wc_results = p.map(self.toolbox.model, self.pop)
-            fitnesses = p.map(self.getFit, wc_results)
+            fitnesses = p.map(self.toolbox.evaluate, wc_results)
 
         # Assign fitness value to each individual in the pop
         for ind, fit in zip(self.pop, fitnesses):
             ind.fitness.values = (fit,)  
 
         fits = [ind.fitness.values[0] for ind in self.pop]
-        print(f"Generation {Generation}, size {len(self.pop)}")
+        print(f"Initial Generation, Size {len(self.pop)}")
         print('Initial Mean Fit: ', np.mean(fits))
         print('Initial Max Fit: ', np.max(fits), ', Parameters: ', self.pop[np.argmax(fits)])
-        print('Initial Pop: ', self.pop)
 
-        while len(self.pop) >= 3:
-            Generation += 1            
-            # elite offspring 
-            elite = self.toolbox.select(self.pop, int(len(self.pop)*self.elitPortion))  
-            elite = list(map(self.toolbox.clone, elite))
+        for Generation in range(self.NGen):  
+            # Select best NPop individuals
+            self.pop = self.toolbox.selBest(self.pop, self.NPop)
+            fits = [ind.fitness.values[0] for ind in self.pop]
             
+            print(f"Generation {Generation}, Size {len(self.pop)}")
+            print('Mean Fit: ', np.mean(fits))
+            print('Max Fit: ', np.max(fits), ', Parameters: ', self.pop[np.argmax(fits)])
+
             # Select crossover offspring
-            crossover = self.toolbox.select(self.pop, int(len(self.pop)*self.crossPortion))  
+            crossover = self.toolbox.selRank(self.pop, self.cx_s)  
             crossover = list(map(self.toolbox.clone, crossover))
             random.shuffle(crossover)
-
             # Select crossover offspring
-            mutants = self.toolbox.select(self.pop, int(len(self.pop)*self.mutPortion))  
+            mutants = self.toolbox.selRank(self.pop, self.mut_s)  
             mutants = list(map(self.toolbox.clone, mutants))
             
             # Apply crossover and mutation
-            self.applyCrossover(crossover)
-            self.applyMutation(mutants)
-
-            # Replace Population with new Individuals
-            self.pop = elite + crossover + mutants
-
+            self.toolbox.mate(crossover)
+            self.toolbox.mutate(mutants)
+            offspring = mutants + crossover 
+            
             # Find invalid fitness values in offspring
-            invalid_ind = [ind for ind in self.pop if not ind.fitness.valid]
+            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
 
             # Reevaluate the fitness of invalid offspring
             with Pool(processes=20) as p:
                 wc_results = p.map(self.toolbox.model, invalid_ind)
-                fitnesses = p.map(self.getFit, wc_results)
+                fitnesses = p.map(self.toolbox.evaluate, wc_results)
 
             # Reasign Fitness Value
             for ind, fit in zip(invalid_ind, fitnesses):
                 ind.fitness.values = (fit,)
-            
-            fits = [ind.fitness.values[0] for ind in self.pop]
-            print(f"Generation {Generation}, size {len(self.pop)}")
-            print('Mean Fit: ', np.mean(fits))
-            print('Max Fit: ', np.max(fits), ', Parameters: ', self.pop[np.argmax(fits)])
-            print('Population: ', self.pop)
+
+            # Merge old and new population
+            self.pop = offspring + self.pop            
         
-    def applyCrossover(self, Individuals):
+    def selRank(self, population, s):
+        """
+        Rank based Selection as described in Introduction to Evolutionary Computing p.82
+        """
+        # Rank fits
+        fits = [ind.fitness.values[0] for ind in population]
+        idx = np.argsort(fits)
+        ranks = np.zeros(len(fits))
+        ranks[idx] = np.arange(len(fits))
+
+        # Calculate Selection Probability and select individuals
+        u =  3
+        selProbs = [(2-s)/u + (2*rank*(s - 1))/(u*(u-1)) for rank in ranks]
+        selected = [ind for prob, ind in zip(selProbs, population) if np.random.rand()<=prob]        
+        return selected
+        
+    def crossover(self, Individuals):
         """
         Applies CrossOver function to list of individuals that is passed in 
-        :return list of individuals 
         """
-        for ind1, ind2 in zip(Individuals[::2], Individuals[1::2]):
-            if np.random.rand() < self.CxPB:
-                (child1, child2) = self.toolbox.mate(ind1, ind2)
-                del child1.fitness.values
-                del child2.fitness.values
+        for child1, child2 in zip(Individuals[::2], Individuals[1::2]):
+            tools.cxBlend(child1, child2, alpha=0.5)
+            # Reset values that are outside ParamRange
+            for child in [child1, child2]: 
+                child[:] = [value if low<=value<=up else min(max(value, low), up) 
+                            for (low, up), value in zip(self.ParamRanges.values(), child[:])]
+                child[:] = np.round(child,4)            
+            del child1.fitness.values
+            del child2.fitness.values
 
-    def applyMutation(self, Individuals):
+    def mutate(self, Individuals):
         """
         Apply Mutation to list of individuals that is passed in
-        :return list of individuals
         """
         for mutant in Individuals:
-            if np.random.rand() < self.MutPB:
-                # indpb is the probability for each attribute to be mutated
-                self.toolbox.mutate(mutant)
-                mutant[:] = np.round(mutant,4)
-                # Reset values that are outside ParamRange
-                mutant[:] = [value if low<=value<=up else min(max(value, low), up) 
-                            for (low, up), value in zip(self.ParamRanges.values(), mutant[:])]
-                del mutant.fitness.values            
+            # Sigma of gaussian distribution with which attributes are mutated
+            mutSigma = [(up-low)/2 for low,up in self.ParamRanges.values()]
+            if mutant.fitness.weights[0]==1.0:
+                mutSigma = list(np.array(1-mutant.fitness.values[0])*np.array(mutSigma))
+            else: 
+                mutSigma = list(np.array(mutant.fitness.values[0])*np.array(mutSigma))
+            # indpb is the probability for each attribute to be mutated                
+            tools.mutGaussian(mutant, mu=0, sigma=mutSigma, indpb=0.5)
+            mutant[:] = np.round(mutant,4)
+            # Reset values that are outside ParamRange
+            mutant[:] = [value if low<=value<=up else min(max(value, low), up) 
+                        for (low, up), value in zip(self.ParamRanges.values(), mutant[:])]
+            del mutant.fitness.values            
 
     def _parallel_wc(self, params):
         """
